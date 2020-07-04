c28effa77a1d3e974a22178f5a7acf9bac597137
TunNandaAung <tunnandaaung@outlook.com>
2020-07-04 20:30:27 +0630

    Add tweet info to tweet card

--- lib/widgets/tweet_card.dart
+++ lib/widgets/tweet_card.dart
@@ -20,37 +20,122 @@ class TweetCard extends StatelessWidget {
           ),
         ],
       ),
-      child: ListTile(
-        contentPadding: EdgeInsets.all(8.0),
-        leading: CircleAvatar(
-          radius: 30.0,
-          backgroundImage: NetworkImage(
-            tweet.user.avatar,
-          ),
-        ),
-        title: RichText(
-          text: TextSpan(
-            text: tweet.user.name,
-            style: Theme.of(context).textTheme.caption,
-            children: [
-              TextSpan(
-                text: "@${tweet.user.username}  " +
-                    timeago.format(tweet.createdAt),
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 14,
-                ),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            contentPadding: EdgeInsets.all(8.0),
+            leading: CircleAvatar(
+              radius: 30.0,
+              backgroundImage: NetworkImage(
+                tweet.user.avatar,
               ),
-            ],
+            ),
+            title: RichText(
+              text: TextSpan(
+                text: tweet.user.name,
+                style: Theme.of(context).textTheme.caption,
+                children: [
+                  TextSpan(
+                    text: "@${tweet.user.username}  " +
+                        timeago.format(tweet.createdAt),
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            subtitle: Padding(
+              padding: EdgeInsets.only(top: 10.0),
+              child: Text(
+                tweet.body,
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+            ),
           ),
-        ),
-        subtitle: Padding(
-          padding: EdgeInsets.only(top: 10.0),
-          child: Text(
-            tweet.body,
-            style: Theme.of(context).textTheme.bodyText1,
+          Divider(
+            color: Colors.grey[300],
           ),
-        ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(90.0, 0.0, 50.0, 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    tweet.likesCount > 0
+                        ? Padding(
+                            padding: EdgeInsets.only(right: 3.0),
+                            child: Text(
+                              tweet.likesCount.toString(),
+                              style: TextStyle(
+                                color: tweet.isLiked
+                                    ? Color(0xFF68D391)
+                                    : Color(0xFFA0AEC0),
+                              ),
+                            ),
+                          )
+                        : Container(),
+                    Icon(
+                      Icons.thumb_up,
+                      size: 18.0,
+                      color:
+                          tweet.isLiked ? Color(0xFF68D391) : Color(0xFFA0AEC0),
+                    ),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    tweet.dislikesCount > 0
+                        ? Padding(
+                            padding: EdgeInsets.only(right: 3.0),
+                            child: Text(
+                              tweet.dislikesCount.toString(),
+                              style: TextStyle(
+                                color: tweet.isDisliked
+                                    ? Color(0xFFE53E3E)
+                                    : Color(0xFFA0AEC0),
+                              ),
+                            ),
+                          )
+                        : Container(),
+                    Icon(
+                      Icons.thumb_down,
+                      size: 18.0,
+                      color: tweet.isDisliked
+                          ? Color(0xFFE53E3E)
+                          : Color(0xFFA0AEC0),
+                    ),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    tweet.repliesCount > 0
+                        ? Padding(
+                            padding: EdgeInsets.only(right: 3.0),
+                            child: Text(
+                              tweet.repliesCount.toString(),
+                              style: TextStyle(
+                                color: Color(0xFFA0AEC0),
+                              ),
+                            ),
+                          )
+                        : Container(),
+                    Icon(
+                      Icons.comment,
+                      size: 18.0,
+                      color: Color(0xFFA0AEC0),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
