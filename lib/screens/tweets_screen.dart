34c83fd497ee722926157cc94ff0062f75771443
TunNandaAung <tunnandaaung@outlook.com>
2020-07-11 22:29:30 +0630

    Add refresh completer in tweets screen

--- lib/screens/tweets_screen.dart
+++ lib/screens/tweets_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -25,6 +27,7 @@ class TweetsScreen extends StatefulWidget {
 class _TweetsScreenState extends State<TweetsScreen> {
   final _scrollController = ScrollController();
   final _scrollThreshold = 200.0;
+  Completer<void> _tweetRefreshCompleter;
 
   @override
   void initState() {
@@ -33,6 +36,7 @@ class _TweetsScreenState extends State<TweetsScreen> {
       FetchTweet(),
     );
     _scrollController.addListener(_onScroll);
+    _tweetRefreshCompleter = Completer<void>();
   }
 
   @override
@@ -58,114 +62,133 @@ class _TweetsScreenState extends State<TweetsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
-      body: CustomScrollView(
-        controller: _scrollController,
-        physics: const BouncingScrollPhysics(
-            parent: AlwaysScrollableScrollPhysics()),
-        slivers: [
-          SliverAppBar(
-            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-            elevation: 20.0,
-            floating: true,
-            iconTheme: IconThemeData(
-              color: Colors.black,
-            ),
-            leading: AvatarButton(
-              scaffoldKey: widget.scaffoldKey,
-            ),
-            title: Text(
-              'Tweety',
-              style: TextStyle(letterSpacing: 1.0, color: Colors.black),
-            ),
-            centerTitle: true,
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.exit_to_app),
-                onPressed: () =>
-                    BlocProvider.of<AuthenticationBloc>(context).add(
-                  AuthenticationLoggedOut(),
+      body: BlocListener<TweetBloc, TweetState>(
+        listener: (context, state) {
+          if (state is TweetLoaded) {
+            _tweetRefreshCompleter?.complete();
+            _tweetRefreshCompleter = Completer();
+          }
+        },
+        child: RefreshIndicator(
+          backgroundColor: Colors.grey[200],
+          color: Colors.black,
+          strokeWidth: 1.0,
+          onRefresh: () {
+            BlocProvider.of<TweetBloc>(context).add(
+              RefreshTweet(),
+            );
+            return _tweetRefreshCompleter.future;
+          },
+          child: CustomScrollView(
+            controller: _scrollController,
+            physics: const BouncingScrollPhysics(
+                parent: AlwaysScrollableScrollPhysics()),
+            slivers: [
+              SliverAppBar(
+                backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                elevation: 20.0,
+                floating: true,
+                iconTheme: IconThemeData(
+                  color: Colors.black,
                 ),
-              )
+                leading: AvatarButton(
+                  scaffoldKey: widget.scaffoldKey,
+                ),
+                title: Text(
+                  'Tweety',
+                  style: TextStyle(letterSpacing: 1.0, color: Colors.black),
+                ),
+                centerTitle: true,
+                actions: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.exit_to_app),
+                    onPressed: () =>
+                        BlocProvider.of<AuthenticationBloc>(context).add(
+                      AuthenticationLoggedOut(),
+                    ),
+                  )
+                ],
+              ),
+              BlocBuilder<TweetBloc, TweetState>(
+                builder: (context, state) {
+                  if (state is TweetLoading) {
+                    return SliverFillRemaining(
+                      child: LoadingIndicator(),
+                    );
+                  }
+                  if (state is TweetLoaded) {
+                    tweets = state.tweets;
+                    if (state.tweets.isEmpty) {
+                      return SliverFillRemaining(
+                        child: Text(
+                          'No tweets yet!',
+                          style: Theme.of(context).textTheme.caption,
+                        ),
+                      );
+                    }
+                    return SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                        (context, index) => index >= tweets.length
+                            ? LoadingIndicator()
+                            : Padding(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 8.0,
+                                  vertical: 5.0,
+                                ),
+                                child: GestureDetector(
+                                  onTap: () {
+                                    final ReplyRepository replyRepository =
+                                        ReplyRepository(
+                                      replyApiClient: ReplyApiClient(
+                                          httpClient: http.Client()),
+                                    );
+                                    Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              BlocProvider<ReplyBloc>(
+                                                create: (context) => ReplyBloc(
+                                                    replyRepository:
+                                                        replyRepository),
+                                                child: TweetScreen(
+                                                    tweet: tweets[index],
+                                                    replyRepository:
+                                                        replyRepository),
+                                              )),
+                                    );
+                                  },
+                                  child: TweetCard(
+                                    tweet: tweets[index],
+                                  ),
+                                ),
+                              ),
+                        childCount: state.hasReachedMax
+                            ? state.tweets.length
+                            : state.tweets.length + 1,
+                      ),
+                    );
+                  }
+                  if (state is TweetError) {
+                    return SliverToBoxAdapter(
+                      child: Container(
+                        child: Refresh(
+                          title: 'Couldn\'t load feed',
+                          onPressed: () {
+                            BlocProvider.of<TweetBloc>(context).add(
+                              RefreshTweet(),
+                            );
+                          },
+                        ),
+                      ),
+                    );
+                  }
+                  return SliverFillRemaining(
+                    child: LoadingIndicator(size: 21.0),
+                  );
+                },
+              ),
             ],
           ),
-          BlocBuilder<TweetBloc, TweetState>(
-            builder: (context, state) {
-              if (state is TweetLoading) {
-                return SliverFillRemaining(
-                  child: LoadingIndicator(),
-                );
-              }
-              if (state is TweetLoaded) {
-                tweets = state.tweets;
-                if (state.tweets.isEmpty) {
-                  return SliverFillRemaining(
-                    child: Text(
-                      'No tweets yet!',
-                      style: Theme.of(context).textTheme.caption,
-                    ),
-                  );
-                }
-                return SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    (context, index) => index >= tweets.length
-                        ? LoadingIndicator()
-                        : Padding(
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 8.0,
-                              vertical: 5.0,
-                            ),
-                            child: GestureDetector(
-                              onTap: () {
-                                final ReplyRepository replyRepository =
-                                    ReplyRepository(
-                                  replyApiClient:
-                                      ReplyApiClient(httpClient: http.Client()),
-                                );
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          BlocProvider<ReplyBloc>(
-                                            create: (context) => ReplyBloc(
-                                                replyRepository:
-                                                    replyRepository),
-                                            child: TweetScreen(
-                                                tweet: tweets[index],
-                                                replyRepository:
-                                                    replyRepository),
-                                          )),
-                                );
-                              },
-                              child: TweetCard(
-                                tweet: tweets[index],
-                              ),
-                            ),
-                          ),
-                    childCount: state.hasReachedMax
-                        ? state.tweets.length
-                        : state.tweets.length + 1,
-                  ),
-                );
-              }
-              if (state is TweetError) {
-                return SliverToBoxAdapter(
-                  child: Container(
-                    child: Refresh(
-                      title: 'Couldn\'t load feed',
-                      onPressed: () {
-                        BlocProvider.of<TweetBloc>(context).add(
-                          RefreshTweet(),
-                        );
-                      },
-                    ),
-                  ),
-                );
-              }
-              return SliverFillRemaining(
-                child: LoadingIndicator(size: 21.0),
-              );
-            },
-          ),
-        ],
+        ),
       ),
     );
   }
